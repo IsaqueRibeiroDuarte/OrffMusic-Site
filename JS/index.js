39c690b6eee3ca8a9d42a117dd7721d60363c697
Isaque Ribeiro <isaqueribeiromcb@gmail.com>
2024-04-28 20:40:28 -0300

    Versão Inicial do Site

---
+++ JS/index.js
@@ -0,0 +1,65 @@
+// Efeito Sonoro Logo
+var tecladoGeral = new Audio();
+tecladoGeral.src = "/AUDIOS/teclado-geral.mp3";
+
+
+// Função Play e Pause onmouseover & onmouseout  
+function play() {
+    play.src = "/AUDIOS/bateria.mp3";
+}
+
+function pause() {
+    pause.src = "";
+}
+
+// Efeito Sonoro Bateria
+var bateria = new Audio();
+bateria.src = "/AUDIOS/bateria.mp3";
+
+// Efeito Sonoro Guitarra
+var guitarra = new Audio();
+guitarra.src = "/AUDIOS/guitarra.mp3";
+
+// Efeito Sonoro Baixo
+var baixo = new Audio();
+baixo.src = "/AUDIOS/baixo.mp3";
+
+// Efeito Sonoro Violino
+var violino = new Audio();
+violino.src = "/AUDIOS/violino.mp3";
+
+// Efeito Sonoro Violão
+var violao = new Audio();
+violao.src = "/AUDIOS/violão.mp3";
+
+// Efeito Sonoro Ukulele
+var ukulele = new Audio();
+ukulele.src = "/AUDIOS/ukulele.mp3";
+
+// Efeito Sonoro Teclado
+var teclado = new Audio();
+teclado.src = "/AUDIOS/teclado.mp3";
+
+// Efeito Sonoro Piano
+var piano = new Audio();
+piano.src = "/AUDIOS/piano.mp3";
+
+// Efeito Sonoro Canto
+var canto = new Audio();
+canto.src = "/AUDIOS/canto.mp3";
+
+// Efeito Sonoro Sax
+var sax = new Audio();
+sax.src = "/AUDIOS/sax.mp3";
+
+// Efeito Sonoro Media Social
+var socialMedia = new Audio();
+socialMedia.src = "/AUDIOS/social-media.mp3";
+
+// Efeito Sonoro Media Social
+var socialMedia2 = new Audio();
+socialMedia2.src = "/AUDIOS/social-media2.mp3";
+
+// Efeito Sonoro Media Social
+var socialMedia3 = new Audio();
+socialMedia3.src = "/AUDIOS/social-media3.mp3";
